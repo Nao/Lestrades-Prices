a3e72c6b85855e6dd1aef29a46fe9ffe7d7b2951
Nao <Nao@users.noreply.github.com>
2025-03-04 16:19:02 +0100

    Update Lestrades-Prices.user.js
    * Seems I have to update the version number every time...?

--- Lestrades-Prices.user.js
+++ Lestrades-Prices.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		 Lestrade's Prices
 // @namespace	https://lestrades.com
-// @version	  0.6
+// @version	  0.61
 // @description  Integrates GG.Deals prices on Lestrades.com with caching, rate limiting, special-item handling, and one-click price lookups.
 // @match		https://lestrades.com/*
 // @connect	  gg.deals
